1a1e1fc3b660ad49b8959939c3e442f2a2d3699a
ypmoraes <67799710+ypmoraes@users.noreply.github.com>
2023-12-20 18:26:03 -0300

    alterando cv

--- src/App.js
+++ src/App.js
@@ -12,13 +12,16 @@ const Resume = () => {
         <h2>Contact Information</h2>
         <p>Email: yuri_ppm@hotmail.com</p>
         <p>LinkedIn: linkedin.com/in/yuripiresmoraes</p>
-        <p>GitHub: github.com/yourusername</p>
+        <p>GitHub: github.com/ypmoraes</p>
       </section>
-      <section className="education">
-        <h2>Education</h2>
+      <section className="About Me">
+        <h2>About Me</h2>
         <p>
-          Bachelor of Science in Computer Science <br />
-          FIAP São Paulo, Graduation Year 2016
+          Cloud Specialist, working with Google, Azure, and AWS,
+          applying the core principles of Dev(sec)Ops and SRE.
+          Experienced with managing critical environments, supporting,
+          and administrating networks, VMs and microservices
+          applications in Cloud, seeking opportunities as a Cloud Engineer
         </p>
       </section>
       <section className="experience">
@@ -27,6 +30,35 @@ const Resume = () => {
           Cloud Specialist <br />
           Cirion | JULY 2023 - Present <br />
           - Developed and maintained web applications using React and Node.js.
+        </p>
+        <p>
+          Cloud Engineer <br />
+          Falconi | MAY 2021 - JUNE 2023 <br />
+          - Developed and maintained web applications using React and Node.js.
+        </p>
+        <p>
+          Cloud Engineer <br />
+          Fujitsu | JUNE 2018 - MARCH 2021 <br />
+          - Developed and maintained web applications using React and Node.js.
+        </p>
+        <p>
+          Help Desk Analyst <br />
+          Burger King | JUNE 2016 - JUNE 2018 <br />
+          - Developed and maintained web applications using React and Node.js.
+        </p>
+      </section>
+      <section className="education">
+        <h2>Education</h2>
+        <p>
+          Computer Networking <br />
+          FIAP São Paulo, Graduation Year 2016
+        </p>
+      </section>
+      <section className="certifications">
+        <h2>Education</h2>
+        <p>
+          Computer Networking <br />
+          FIAP São Paulo, Graduation Year 2016
         </p>
       </section>
       <section className="skills">
